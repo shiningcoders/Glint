eaf7844ef283453d0ac2ef8621ff98f0c0ffa55a
Ajayraj Singh <shiningcoders@gmail.com>
2020-08-16 07:29:54 +0530

    Added Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(Glint());
 
 class Glint extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
       title: 'Glint',
-      home: StartingPoint(),
+      home: SplashScreen('assets/animations/splash.flr', StartingPoint(),
+          startAnimation: 'Intro', backgroundColor: Colors.white),
     );
   }
 }
