f2aac9d1a9c9faed634fa555a447b91b265e2378
Ajayraj Singh <shiningcoders@gmail.com>
2020-08-16 12:46:44 +0530

    Added Device Preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,28 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flare_splash_screen/flare_splash_screen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:glint/Onboard/welcome.dart';
 
-void main() => runApp(Glint());
+void main() => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => Glint(),
+      ),
+    );
 
 class Glint extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([]);
     return MaterialApp(
+      locale: DevicePreview.of(context).locale,
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
       title: 'Glint',
-      home: SplashScreen('assets/animations/splash.flr', StartingPoint(),
+      home: SplashScreen('assets/animations/splash.flr', WelcomeScreen(),
           startAnimation: 'Intro', backgroundColor: Colors.white),
     );
-  }
-}
-
-class StartingPoint extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
